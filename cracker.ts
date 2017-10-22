127c83cabb8eb06941e838c399f6aab1eb5ea749
Joshua Jolley <jolleyboy@gmail.com>
2017-10-22 12:50:10 -0600

    t1 lock cracking script updated

--- cracker.ts
+++ cracker.ts
@@ -1,50 +1,70 @@
-function (t, v) {
-    let f = v.f, r = { r: f.call({}), o: {} }, h = (a, b) => new RegExp(b, "ig").test(a), g, x, j, k, p, n, l, u, o, q, w, z, s
-    w = ['ez_21', 'ez_35', 'ez_40', 'c001', 'c002', 'c003', 'l0cket']
-    let [a, b, c, d, e, i, y] = w
-    j = ['open', 'unlock', 'release']
-    k = ['red', 'purple', 'blue', 'cyan', 'green', 'lime', 'yellow', 'orange']
-    s = ['tvfkyq', 'vc2c7q']
-    n = new Array(10).keys()
-    p = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
-    q = (s, n) => s.split('\n').slice(n)
-    g = (k, s, l, a = {}) => {
-        let r, x, o
-        for (let x of s) {
-            o = Object.assign({}, a, { [k]: x })
-            r = f.call(o)
-            x = q(r, -3).join('\n')
-            if (h(x, l) || h(x, 'rmi'))
-                return { r, o }
+function (context, args) {
+    let state = {
+        results: args.f.call({}),
+        argument: {},
+        failed: 0
+    }
+
+    let locks = ['ez_21', 'ez_35', 'ez_40', 'c001', 'c002', 'c003', 'l0cket'];
+    let ez_passwords = ['open', 'unlock', 'release'];
+    let colors = ['red', 'purple', 'blue', 'cyan', 'green', 'lime', 'yellow', 'orange'];
+    let k3ys = ['tvfkyq', 'vc2c7q']; //only 6 more to find! 
+    let digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    let primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+
+    // does string have phrase in it?
+    let includes = (string, phrase) => new RegExp(phrase, "ig").test(string);
+
+    // get the last n lines
+    let getLastNLines = (str, n) => str.split('\n').slice(-n)
+
+    // advance the argument 
+    let evolveArgument = (prop, values, lock, argument = {}) => {
+        let results, last3
+        for (let value of values) {
+            argument = {...argument, [prop]: value}
+            results = args.f.call(argument);
+            last3 = getLastNLines(results, 3) 
+            if (includes(last3, lock) || includes(last3, 'nn'))
+                return {results, argument, failed: 0}
         }
-        return { r, o, f: 1 }
+        return {results, argument, failed: 1}
     }
-    l = (s) => {
-        for (let l of w)
-            if (h(s, l))
-                return l
+
+    // figure out which lock we're on
+    let determineTheCurrentLock = (lastLine) => {
+        for (let lock of locks)
+            if (includes(lastLine, lock))
+                return lock
     }
-    u = (l, o) => {
-        if (l == a)
-            return g(l, j, l, o)
-        if (l == b)
-            return g('digit', n, l, g(l, j, ['ig'], o).o)
-        if (l == c)
-            return g('ez_prime', p, l, g(l, j, ['_p'], o).o)
-        if (l == d)
-            return g('color_digit', n, l, g(l, k, ['_d'], o).o)
-        if (l == e)
-            return g('c002_complement', k, l, g(l, k, ['2_'], o).o)
-        if (l == i)
-            return g('c003_triad_2', k, l, g('c003_triad_1', k, ['d_2'], g(l, k, ['_1'], o).o).o)
-        if (l == y)
-            return g(l, s, l, o)
-        return 'l0cket'
+
+    // solve at the lock level.  It might be better/faster in the future to solve on the 'step' level
+    let unlock = (lock, argument) => {
+        if (lock == locks[0]) 
+            return evolveArgument(lock, ez_passwords, lock, argument)
+        if (lock == locks[1])
+            return evolveArgument('digit', digits, lock, evolveArgument(lock, ez_passwords, ['ig'], argument).argument)
+        if (lock == locks[2])
+            return evolveArgument('ez_prime', primes, lock, evolveArgument(lock, ez_passwords, ['_p'], argument).argument)
+        if (lock == locks[3])
+            return evolveArgument('color_digit', digits, lock, evolveArgument(lock, colors, ['_d'], argument).argument )
+        if (lock == locks[4])
+            return evolveArgument('c002_complement', colors, lock, evolveArgument(lock, colors, ['2_'], argument).argument)
+        if (lock == locks[5])
+            return evolveArgument('c003_triad_2', colors, lock, evolveArgument('c003_triad_1', colors, ['d_2'], evolveArgument(lock, colors, ['_1'], argument).argument).argument)
+        if (lock == locks[6])
+            return evolveArgument(lock, k3ys, lock, argument)
+        return {results: lock, argument, failed: 1}
     }
-    z = [r]
-     while (!r.f && r.r && !/rmi/gi.test(r.r)) {
-        r = u(l(q(r.r, -1)[0]), r.o)
-        z.push(r)
+
+    // This occurs when the script doesn't exist
+    if (typeof state.results === 'object') {
+        return state.results }
+
+    // Loop through each lock until we get 'Connection terminated.'
+    while(!state.failed && state.results && !includes(state.results, 'nn')) {
+         state = unlock(determineTheCurrentLock(getLastNLines(state.results, 1)[0]), state.argument)
     }
-    return r
+
+    return state;
 }
